a810d520402b80e93eff685978e32b563af8b867
tokuyama shouta <toku1031ts@gmail.com>
2020-12-25 21:51:26 +0900

    ユーザープロフィール機能

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from "./Auth.module.css"
 import { useDispatch } from "react-redux"
+import { updateUserProfile } from "../features/userSlice"
 import { auth, provider, storage} from "../firebase"
 
 import {
@@ -57,15 +58,48 @@ const useStyles = makeStyles((theme) => ({
 
 const Auth:React.FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUserName] = useState("");
+  const [avatarImage, setAvatarImage] = useState<File | null>(null);
   const [isLogin, setIsLogin] = useState(true);
+
+  const onChangeImageHandler =(e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setAvatarImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
 
   const signInEmail = async () => {
     await auth.signInWithEmailAndPassword(email, password);
   };
   const signUpEmail = async () => {
-    await auth.createUserWithEmailAndPassword(email, password);
+    const authUser = await auth.createUserWithEmailAndPassword(email, password);
+    let url = "";
+    if( avatarImage ) {
+      const S =
+        "abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+      const fileName = randomChar + "_" + avatarImage.name;
+
+      await storage.ref(`avatars/${fileName}`).put(avatarImage)
+      url = await storage.ref("avatars").child(fileName).getDownloadURL();
+    }
+    await authUser.user?.updateProfile({
+      displayName: username,
+      photoURL: url,
+    });
+    dispatch(
+      updateUserProfile({
+        displayName: username,
+        photoUrl: url,
+      })
+    );
   };
 
   const signInGoogle = async () => {
@@ -145,7 +179,7 @@ const Auth:React.FC = () => {
               <Grid item xs>
                 <span className={styles.login_reset} >Forgot password?</span>
               </Grid>
-              <Grid item xs>
+              <Grid item>
                 <span 
                   className={styles.login_toggleMode} 
                   onClick={() => setIsLogin(!isLogin)}
